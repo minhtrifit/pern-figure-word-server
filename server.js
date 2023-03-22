3174f4bf4f7e9486a85e2ab7edcdeed801dbaa2a
minhtrifit <minhtri.fit@gmail.com>
2023-03-22 20:25:06 +0700

    update cors local

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader(
     "Access-Control-Allow-Headers",
